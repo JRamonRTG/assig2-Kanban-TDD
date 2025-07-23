69133ba39dd28096aa5a79111f3a2a0bdec83962
JRamonRTG <ramon.rivasterraza@galileo.edu>
2025-07-22 22:04:06 -0600

    actulizacion de tests, ya que por alguna razon el archivo anterior no se subio con la libreria pytest, este utiliza unittest y contextlib

---
+++ testsfizzbuzz.py
@@ -0,0 +1,14 @@
+import unittest
+from io import StringIO
+from contextlib import redirect_stdout
+from fizzbuzz import fizzbuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def capturar_salida(self, n):
+        buffer = StringIO()
+        with redirect_stdout(buffer):
+            fizzbuzz(n)
+        return buffer.getvalue()
+
+unittest.main()
